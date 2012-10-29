07a7e62da1f5d4bdfdf33a99d66ba257fb3075df
Salma <srodr063@fiu.edu>
2012-10-29 17:17:19 -0400

    updating test.c

--- daemon-apps/test.c
+++ daemon-apps/test.c
@@ -9,10 +9,15 @@ int main(int argc, char **argv) {
 	char s;
 	int i, fd;
 	s = '1';
+	if (argc != 1) {
+		perror("usage: test <no of iterations>");
+		exit(0);
+	}
 	for (i = 0; i < atoi(argv[1]); i++) {
 		fd = open("/media/dmmount/file", O_WRONLY | O_CREAT, 0666);
 		write(fd, &s, 1);
 		close(fd);
 		sleep(61);
 	}
+	return 0;
 }
